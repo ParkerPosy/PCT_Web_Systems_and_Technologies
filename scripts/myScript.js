ffec8ba1c3280623719b5b1fde088758c63a3044
Parker <parkerconn@gmail.com>
2021-03-01 13:07:40 -0500

    initial commit

---
+++ scripts/myScript.js
@@ -0,0 +1,25 @@
+$("document").ready(function () {
+  let trigger = $("#hamburger")
+  let isClosed = true
+  let menu = $('#navbar-buttons')
+
+  trigger.click(function () {
+    burgerTime();
+  });
+
+  function burgerTime() {
+    if (isClosed == true) {
+      trigger.removeClass("is-open");
+      trigger.addClass("is-closed");
+      menu.removeClass('expanded');
+      menu.addClass('closed');
+      isClosed = false;
+    } else {
+      trigger.removeClass("is-closed");
+      trigger.addClass("is-open");
+      menu.removeClass('closed');
+      menu.addClass('expanded');
+      isClosed = true;
+    }
+  }
+});
