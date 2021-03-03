be88f41ab2933dff726ceb58f217e96a87ec4a25
Parker <parkerconn@gmail.com>
2021-03-03 09:44:05 -0500

    clean up code

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,24 +1,20 @@
 $("document").ready(function () {
-  let trigger = $("#hamburger")
-  let isClosed = false
-  let menu = $('#navbar-buttons')
+  let trigger = $("#hamburger");
+  let isClosed = false;
+  let menu = $("#navbar-buttons");
 
   trigger.click(function () {
     burgerTime();
   });
 
   function burgerTime() {
-    if (isClosed == true) {
-      trigger.removeClass("is-open");
-      trigger.addClass("is-closed");
-      menu.removeClass('expanded');
-      menu.addClass('closed');
+    if (isClosed) {
+      trigger.removeClass("is-open").addClass("is-closed");
+      menu.removeClass("expanded").addClass("closed");
       isClosed = false;
     } else {
-      trigger.removeClass("is-closed");
-      trigger.addClass("is-open");
-      menu.removeClass('closed');
-      menu.addClass('expanded');
+      trigger.removeClass("is-closed").addClass("is-open");
+      menu.removeClass("closed").addClass("expanded");
       isClosed = true;
     }
   }
